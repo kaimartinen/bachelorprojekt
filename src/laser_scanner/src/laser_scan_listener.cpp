27b16ac3388df2d189fb358d3a9b4788f2d04c8a
Kai <kai-martinen@msn.com>
2017-01-19 15:36:39 +0100

    point cloud published now

--- src/laser_scanner/src/laser_scan_listener.cpp
+++ src/laser_scanner/src/laser_scan_listener.cpp
@@ -2,26 +2,26 @@
 #include "sensor_msgs/LaserScan.h"
 #include "sensor_msgs/PointCloud.h"
 #include "laser_geometry/laser_geometry.h"
-#include "point_cloud_publisher/point_cloud_publisher.h"
 
 
 laser_geometry::LaserProjection projector;
-ros::NodeHandle m;
-Point_Cloud_Publisher* publisher = new Point_Cloud_Publisher(&m, "Laser_Point_Cloud");
+ros::Publisher _publisher;
 
 void LaserCallback(const sensor_msgs::LaserScan::ConstPtr& scan_in) {
 
   sensor_msgs::PointCloud cloud;
+  //ROS_INFO("Angle increment of laser: %lf\n", scan_in->angle_increment);
   projector.projectLaser(*scan_in, cloud);
-  publisher->publish(cloud);
+  _publisher.publish(cloud);
 }
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "laser_scan_listener");
 
   ros::NodeHandle n;
-
-  ros::Subscriber sub = n.subscribe("base_laser_link", 1000, LaserCallback);
+  ros::NodeHandle m;
+  _publisher = n.advertise<sensor_msgs::PointCloud>("Laser_Point_Cloud", 1000);
+  ros::Subscriber sub = n.subscribe("base_scan", 1000, LaserCallback);
 
   ros::spin();
 
