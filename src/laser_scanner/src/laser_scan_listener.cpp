7d96eec28e48c9133fdda695dc277b9f13b80f7b
Kai <kai-martinen@msn.com>
2017-01-26 15:10:12 +0100

    not working

--- src/laser_scanner/src/laser_scan_listener.cpp
+++ src/laser_scanner/src/laser_scan_listener.cpp
@@ -2,9 +2,10 @@
 #include "sensor_msgs/LaserScan.h"
 #include "sensor_msgs/PointCloud.h"
 #include "laser_geometry/laser_geometry.h"
+#include "laser_line_extraction/line_extraction_ros.h"
 
 
-laser_geometry::LaserProjection projector;
+/*laser_geometry::LaserProjection projector;
 ros::Publisher _publisher;
 
 void LaserCallback(const sensor_msgs::LaserScan::ConstPtr& scan_in) {
@@ -13,17 +14,33 @@ void LaserCallback(const sensor_msgs::LaserScan::ConstPtr& scan_in) {
   //ROS_INFO("Angle increment of laser: %lf\n", scan_in->angle_increment);
   projector.projectLaser(*scan_in, cloud);
   _publisher.publish(cloud);
-}
+
+}*/
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "laser_scan_listener");
 
-  ros::NodeHandle n;
+  /*ros::NodeHandle n;
   ros::NodeHandle m;
-  _publisher = n.advertise<sensor_msgs::PointCloud>("Laser_Point_Cloud", 1000);
-  ros::Subscriber sub = n.subscribe("base_scan", 1000, LaserCallback);
+  _publisher = m.advertise<sensor_msgs::PointCloud>("Laser_Point_Cloud", 1000);
+  ros::Subscriber sub = n.subscribe("base_scan", 1000, LaserCallback);*/
 
-  ros::spin();
+  //ros::spin();
+
+  ros::NodeHandle nh;
+  ros::NodeHandle nh_local("~");
+  line_extraction::LineExtractionROS line_extractor(nh, nh_local);
+
+  /*double frequency;
+  nh_local.param<double>("frequency", frequency, 25);
+  ros::Rate rate(frequency);
+
+  while (ros::ok())
+  {
+    line_extractor.run();
+    ros::spinOnce();
+    rate.sleep();
+  }*/
 
   return 0;
 }
